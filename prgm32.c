8e70c8553638e75cb19a1453faa15b126edcf0d3
suryasadhana <suryasadhana74@gmail.com>
2017-06-29 11:13:16 +0530

    Create prgm32.c

---
+++ prgm32.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+int i=0;f=1,l=0;
+clrscr();
+puts("enter a string");
+gets(str);
+for(i=0;i<=l-1;i++)
+{
+f=f+1;
+}
+printf("%d",f);
+getch();
+}
